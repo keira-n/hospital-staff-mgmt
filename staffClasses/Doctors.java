d4b45cce034e0fedc990a5c0f1bb5d219f8f527c
Keira <94308338+KeiraYeet@users.noreply.github.com>
2025-10-29 23:23:39 +0700

    test

---
+++ staffClasses/Doctors.java
@@ -0,0 +1,17 @@
+package staffClasses;
+import java.util.*;
+
+class Doctor{
+    void getDetails(){
+        System.out.println("Treat patients");
+    }
+    void getWorkingDays(){
+        for(i = 2, i <= 8, i++){
+            if (){}
+        }
+    }
+}
+
+
+public class Doctors {
+}
